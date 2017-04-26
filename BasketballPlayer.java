e72101be76d40f86ad31444395d665fee20ff481
jaime17 <A01411527@itesm.mx>
2017-04-26 17:55:05 -0500

    Create BasketballPlayer.java

---
+++ BasketballPlayer.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main2;
+
+/**
+ *
+ * @author jaime
+ */
+public class BasketballPlayer extends player{
+    
+    public BasketballPlayer(String name) {
+        super(name);
+    }
+    
+}
